8f8b6dacd69be62e1cc3ec54f842e8fda13dbacf
masfuego <75271386+masfuego@users.noreply.github.com>
2022-04-30 11:42:37 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+import 'dart:io';
+
+void main(){
+  print('Enter a number: ');
+  String? num1 = stdin.readLineSync();
+  if (num1 == null){ num1 = '0';}
+  print('Enter second number:');
+  String? num2 = stdin.readLineSync();
+  if (num2 == null){ num2 = '0';}
+  print(num1+num2);
+}
