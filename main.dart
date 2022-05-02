eedd49152c755efd74ea5fa182cae2fd25c4b22e
masfuego <75271386+masfuego@users.noreply.github.com>
2022-05-02 10:06:49 +0000

    string prompt is a func with parameter String promptText and returns answer. - this answer entered by the user is then stored in a variable colour - and then prints colour

--- main.dart
+++ main.dart
@@ -1,12 +1,22 @@
 import 'dart:math';
 import 'dart:io';
 
-double addNumbers(double num1, double num2){
-  print('order of execution, return keyword last in function');
-  return num1 + num2;
+String prompt(String promptText){
+  print(promptText);
+  String answer = stdin.readLineSync()!;
+  return answer;
 }
-
 
 void main(){
-  print(addNumbers(5, 10));
+
+  String colour = prompt('enter a color: ');
+  print(colour);
+  
 }
+
+//to fix the following error:
+//Error: A value of type 'String?' can't be assigned to a variable of type 'String' because 'String?' is nullable and 'String' isn't.  String answer = stdin.readLineSync();
+//I guess you are using null safety. The error occurs because stdin.readLineSync() returns a String? but you assing it on String name. To fix it add a ? or a !, but then make sure it is not null.
+
+//String? name = stdin.readLineSync();
+//String name = stdin.readLineSync()!;
